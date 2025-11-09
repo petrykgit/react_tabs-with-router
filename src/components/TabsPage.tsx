9d09b29dcad7300fb63dae6bc7b3a46ad7a3cc5e
Ievgen Petryk <jakeshad123@gmail.com>
2025-11-10 00:14:16 +0200

    fixed

--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,27 +1,23 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
-import { TabItem } from './TabItem';
+import type { Tab as TabType } from '../types/Tab';
+import { Tabs } from './Tabs';
 
-const tabs: Tab[] = [
+const tabs: TabType[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const TabsPage = () => {
-  const { tabId } = useParams();
-  const activeTab = tabs.find(tab => tab.id === tabId);
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams<{ tabId?: string }>();
+  const activeTab = tabs.find(t => t.id === tabId);
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <TabItem key={tab.id} tab={tab} tabId={tabId} />
-          ))}
-        </ul>
-      </div>
+
+      <Tabs tabs={tabs} activeTabId={tabId} />
 
       <div className="block" data-cy="TabContent">
         {activeTab ? activeTab.content : 'Please select a tab'}
